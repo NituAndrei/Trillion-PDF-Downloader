2237804d22d893b14728f990492bae26c4a5c62c
Nitu Andrei <nituandrei98@gmail.com>
2019-03-07 19:38:46 +0200

    main.py
    Now can download pdfs

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 from bs4 import BeautifulSoup
 import urllib.request
+import urllib.parse
 
 #choose which pdfs to download (ex: 1-6, 10, 12)
 #parse links to downloader function
 
+def downloader(pdf_links):
+    dl_path = 'C:/Users/Y530/Downloads/python test'
+    for pdf_link in pdf_links:
+        dl_link=urllib.parse.urljoin(url,pdf_link) #turns links into true urls
+        pdf_name=pdf_link.split('/')
+        output_file=open(dl_path + '/' + pdf_name[len(pdf_name)-1], 'wb')
+        output_file.write((urllib.request.urlopen(dl_link).read()))
+
+#url='http://www.os-book.com/OSE2/practice-exer-dir/index.html'
 url='http://www.os-book.com/OSE2/practice-exer-dir/index.html'
 webpage = urllib.request.urlopen(url)
 webpage_src = webpage.read()
@@ -15,8 +25,7 @@ for link in soup.findAll("a"):
     link_url[len(link_url)-2]=='d' and
     link_url[len(link_url)-3]=='p' and
     link_url[len(link_url)-4]=='.'): #checked if link ends in .pdf
-        print(link_url)
-        pdf_links.append(link_url) #watch out for relative paths
+        pdf_links.append(link_url) #pdf names
 
-for i in pdf_links:
-    print(i)
+
+downloader(pdf_links)
