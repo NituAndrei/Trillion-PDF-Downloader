7c7c9bad065d2d221c265f746f13ea5a498a74a3
Nitu Andrei <nituandrei98@gmail.com>
2019-03-14 14:19:46 +0200

    main.py
    work on selector

--- main.py
+++ main.py
@@ -2,30 +2,56 @@ from bs4 import BeautifulSoup
 import urllib.request
 import urllib.parse
 
-#choose which pdfs to download (ex: 1-6, 10, 12)
-#parse links to downloader function
+
+# choose which pdfs to download (ex: 1-6, 10, 12)
+# parse links to downloader function
 
 def downloader(pdf_links):
     dl_path = 'C:/Users/Y530/Downloads/python test'
     for pdf_link in pdf_links:
-        dl_link=urllib.parse.urljoin(url,pdf_link) #turns links into true urls
-        pdf_name=pdf_link.split('/')
-        output_file=open(dl_path + '/' + pdf_name[len(pdf_name)-1], 'wb')
+        dl_link = urllib.parse.urljoin(url, pdf_link)  # turns links into true urls
+        pdf_name = pdf_link.split('/')
+        output_file = open(dl_path + '/' + pdf_name[len(pdf_name) - 1], 'wb')
         output_file.write((urllib.request.urlopen(dl_link).read()))
 
-#url='http://www.os-book.com/OSE2/practice-exer-dir/index.html'
-url='http://www.os-book.com/OSE2/practice-exer-dir/index.html'
+
+def selector(pdf_links):
+    chosen_pdfs = []
+    numbers = '0123456789'
+    print('Select pdf indexes (format: 1-6,10,20,25-40)')
+    selection = input()
+    if (len(selection) == 0):
+        return 0
+    i = 0
+    while i < len(selection):
+        if selection[i] in numbers:
+            a = 0
+            while i < len(selection) and selection[i] in numbers:
+                a = a * 10 + int(selection[i])
+                i = i + 1
+            print(a, i)
+            # todo: case - and ,
+        else:
+            print('invalid input')
+            return -1
+        i = i + 1
+
+    return pdf_links
+
+
+# url='http://www.os-book.com/OSE2/practice-exer-dir/index.html'
+url = 'http://www.os-book.com/OSE2/practice-exer-dir/index.html'
 webpage = urllib.request.urlopen(url)
 webpage_src = webpage.read()
 soup = BeautifulSoup(webpage_src)
 pdf_links = []
 for link in soup.findAll("a"):
-    link_url=link.attrs['href']
-    if( link_url[len(link_url)-1]=='f' and
-    link_url[len(link_url)-2]=='d' and
-    link_url[len(link_url)-3]=='p' and
-    link_url[len(link_url)-4]=='.'): #checked if link ends in .pdf
-        pdf_links.append(link_url) #pdf names
+    link_url = link.attrs['href']
+    if link_url[len(link_url) - 4: len(link_url)] == '.pdf':
+        pdf_links.append(link_url)  # pdf names
 
+for i in range(len(pdf_links)):
+    print('index:', i, pdf_links[i])
 
-downloader(pdf_links)
+pdf_links = selector(pdf_links)
+# downloader(pdf_links)
